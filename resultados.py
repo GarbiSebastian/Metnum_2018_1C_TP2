c340134da776ee324f144129a5effe40bf5fd006
GarbiSebastian <garbyseba@gmail.com>
2018-05-09 16:59:20 -0300

    prec_rec

--- resultados.py
+++ resultados.py
@@ -14,6 +14,8 @@ resultados = [[0 for i in range(casos)] for y in range(sujetos)]
 confusion = [[0 for i in range(sujetos)] for y in range(sujetos)]
 total = 0
 x_sujetos = [0 for i in range(sujetos)]
+positives = [0 for i in range(sujetos)]
+#negatives = [0 for i in range(sujetos)]
 
 def datos(i,tipo=TP):
     return resultados[i][tipo]
@@ -26,6 +28,7 @@ for line in fileinput.input():
     obtenido = int(item[1])-1
     confusion[real][obtenido]+=1
     x_sujetos[real]+=1
+    positives[obtenido]+=1
     if real==obtenido:
         resultados[real][TP]+=1
     else:
@@ -39,67 +42,70 @@ for i in range(sujetos):
         print(resultados[i][j], end=' ', flush=True)
     print('',flush=True)"""
 
-"""for i in range(sujetos):
-    for j in range(sujetos):
-        print(confusion[i][j], end=' ', flush=True)
-    print('',flush=True)"""
-"""x = numpy.arange (start=2, stop=120, step=8)
-print(x,flush=True)"""
-
-g = Gnuplot.Gnuplot()
-g.title("Resultados")
-g.xlabel("Sujetos")
-g.ylabel("Resultados")
-
-#g("set terminal png giant font 'Helvetica' 16")
-g("set terminal png size 1000, 500")
-g("set key below")
-
-g("set grid y")
-
-g("set style data histograms")
-g("set style histogram rowstacked")
-g("set boxwidth 0.5")
-g("set style fill solid 1.0 border -1")
-
-g("set ytics 10 nomirror")
-g("set yrange [0:100]")
-g("set ytics 10")
-
-g("set xtics 1")
-#g("set xtic 1")
-xtics=", ".join(["'"+str(i+1)+"' "+str(i) for i in range(sujetos)])
-#print xtics
-
-g("set xtics("+xtics+")")
+def gBars(sujetos,xlabel="Sujetos",ylabel="Resultados",title="Resultados"):
+    g = Gnuplot.Gnuplot()
+    g.title(title)
+    g.xlabel(xlabel)
+    g.ylabel(ylabel)
+    g("set terminal png size 1000, 500")
+    g("set key below")
+    g("set grid y")
+    g("set style data histograms")
+    g("set style histogram rowstacked")
+    g("set boxwidth 1")
+    g("set style fill solid 1.0 border -1")
+    g("set ytics 10 nomirror")
+    #g("set yrange [0:100]")
+    g("set ytics 10")
+    g("set xtics 1")
+    xtics=", ".join(["'"+str(i+1)+"' "+str(i) for i in range(sujetos)])
+    g("set xtics("+xtics+")")
+    g("set xrange [-1:"+str(sujetos)+"]")
+    g("xequiv=100")
+    return g
 
 
-g("set xrange [-1:41]")
-#g("xspan = GPVAL_DATA_X_MAX - GPVAL_DATA_X_MIN")
-g("xequiv=100")
+def plotRecall(sujetos,cantidades):
+    g = gBars(sujetos,title="Recall")
+    x=range(sujetos)
+    y1=[ (float(datos(i,tipo=TP))/cantidades[i])*100 for i in x]
+    #y2=[ (float(datos(i,tipo=FN))/cantidades[i])*100 for i in x]
+    d1 = Gnuplot.Data(x,y1,using="2",title="Recall(tp/tp+fn)")
+    #d2 = Gnuplot.Data(x,y2,using="2",title="Resto(fn/tp+fn)")
+    g("set output 'recall.png'")
+    #g.plot(d1,d2)
+    g.plot(d1)
+    del g
 
+def plotPresicion(sujetos,cantidades):
+    g = gBars(sujetos,title="Presicion")
+    x=range(sujetos)
+    y1=[ (float(datos(i,tipo=TP))/cantidades[i])*100 for i in x]
+    #y4=[ (float(datos(i,tipo=FN))/x_sujetos[i])*100 for i in x]
+    d1 = Gnuplot.Data(x,y1,using="2",title="Presicion(tp/tp+fp)")
+    #d2 = Gnuplot.Data(x,y2,using="2",title="Resto(fn/tp+fn)")
+    g("set output 'presicion.png'")
+    g.plot(d1)
+    del g
 
-x1=range(sujetos)
-xlabels=[i+1 for i in x1]
-y1=[ (float(datos(i,tipo=TP))/x_sujetos[i])*100 for i in x1]
-y2=[ (float(datos(i,tipo=FP))/(total- x_sujetos[i]))*100 for i in x1]
-y3=[ (float(datos(i,tipo=TN))/(total- x_sujetos[i]))*100 for i in x1]
-y4=[ (float(datos(i,tipo=FN))/x_sujetos[i])*100 for i in x1]
+def mediaArmonica():
+    print 1
 
-"""print x1
-print x_sujetos
-print y1
-print y2
-print y3
-print y4"""
+plotRecall(sujetos,cantidades=x_sujetos)
+plotPresicion(sujetos,cantidades=positives)
 
-d1 = Gnuplot.Data(xlabels,y1,using="2",title="Positivos correctos")
-d2 = Gnuplot.Data(xlabels,y2,using="2",title="Positivos incorrectos")
-d3 = Gnuplot.Data(xlabels,y3,using="2",title="Negativos correctos")
-d4 = Gnuplot.Data(xlabels,y4,using="2",title="Negativos incorrectos")
+"""y1=[ (float(datos(i,tipo=TP))/x_sujetos[i])*100 for i in x]
+y2=[ (float(datos(i,tipo=FP))/(total- x_sujetos[i]))*100 for i in x]
+y3=[ (float(datos(i,tipo=TN))/(total- x_sujetos[i]))*100 for i in x]
+y4=[ (float(datos(i,tipo=FN))/x_sujetos[i])*100 for i in x]"""
 
-g("set output 'resultados_TP_FN.png'")
-g.plot(d1,d4)
-g("set output 'resultados_TN_FP.png'")
-g.plot(d3,d2)
-del g
+"""d1 = Gnuplot.Data(x,y1,using="2",title="Positivos correctos")
+d2 = Gnuplot.Data(x,y2,using="2",title="Positivos incorrectos")
+d3 = Gnuplot.Data(x,y3,using="2",title="Negativos correctos")
+d4 = Gnuplot.Data(x,y4,using="2",title="Negativos incorrectos")"""
+
+#g("set output 'resultados_TP_FN.png'")
+#g.plot(d1,d4)
+#g("set output 'resultados_TN_FP.png'")
+#g.plot(d3,d2)
+#del g
