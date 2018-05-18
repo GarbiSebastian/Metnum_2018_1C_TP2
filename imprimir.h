8d982c90b7775ad30b22ccb40924b249f8a70c31
cronos <cronos@cronos-UL30A>
2018-05-18 19:44:36 -0300

    Fix imprimir

--- imprimir.h
+++ imprimir.h
@@ -5,12 +5,28 @@
 
 using namespace std;
 
-//template<typename T> 
-//void imprimir(vector< vector< T > > &m,ostream &out){
-//	for(unsigned int i=0; i< m.size();i++){
-//		imprimir(m[i],out);
-//	}
-//}
+template<typename T>
+void imprimir(vector< vector< T > > &m, ostream &out) {
+    for (unsigned int i = 0; i < m.size(); i++) {
+        vector< T > v = m[i];
+        for (unsigned int j = 0; j < v.size(); j++) {
+            out << v[j] << " ";
+        }
+        out << endl;
+    }
+}
+
+template<typename T>
+void imprimirUchar(vector< vector< T > > &m, ostream &out) {
+    for (unsigned int i = 0; i < m.size(); i++) {
+        vector< T > v = m[i];
+        for (unsigned int j = 0; j < v.size(); j++) {
+            out << (int) v[j] << " ";
+        }
+        out << endl;
+    }
+}
+
 //
 //template<typename T> 
 //void imprimir(vector< vector< T > > &m){
@@ -18,32 +34,50 @@ using namespace std;
 //}
 
 template<typename T>
-void imprimir(vector<T> &v,ostream &out, bool transpuesto){
-	out << scientific ;
-	if(transpuesto){
-		for(unsigned int i = 0; i < v.size();i++){
-//			out << (double)v[i] << endl; 
-			out << v[i] << endl; 
-		}
-	}else{
-//		out << (double)v[0];
-		out << v[0];
-		for(unsigned int i = 1; i < v.size();i++){
-//			out << " " << (double)v[i]; 
-			out << " " << v[i];
-		}
-		out << endl;
-	}
+void imprimir(vector<T> &v, ostream &out, bool transpuesto) {
+    out << scientific;
+    if (transpuesto) {
+        for (unsigned int i = 0; i < v.size(); i++) {
+            //			out << (double)v[i] << endl; 
+            out << v[i] << endl;
+        }
+    } else {
+        //		out << (double)v[0];
+        out << (int) v[0];
+        for (unsigned int i = 1; i < v.size(); i++) {
+            //			out << " " << (double)v[i]; 
+            out << " " << v[i];
+        }
+        out << endl;
+    }
 }
 
 template<typename T>
-void imprimir(vector<T> &v,ostream &out){
-	imprimir(v,out,false);
+void imprimirUchar(vector<T> &v, ostream &out, bool transpuesto) {
+    out << scientific;
+    if (transpuesto) {
+        for (unsigned int i = 0; i < v.size(); i++) {
+            //			out << (double)v[i] << endl; 
+            out << (int)v[i] << endl;
+        }
+    } else {
+        //		out << (double)v[0];
+        out << (int) v[0];
+        for (unsigned int i = 1; i < v.size(); i++) {
+            //			out << " " << (double)v[i]; 
+            out << " " << (int)v[i];
+        }
+        out << endl;
+    }
 }
 
 
+template<typename T>
+void imprimir(vector<T> &v, ostream &out) {
+    imprimir(v, out, false);
+}
 
 template<typename T>
-void imprimir(vector<T> &v){
-	imprimir(v,cout);
+void imprimir(vector<T> &v) {
+    imprimir(v, cout);
 }
